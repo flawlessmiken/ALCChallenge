18c69d149407d0bf8843f2e258c62e8b7013f5ad
flawlessmiken <honmichael.5@gmail.com>
2019-07-15 05:17:57 +0100

    Third Commit

--- app/src/main/java/com/example/alc5dayschallenge/AboutAlcActivity.java
+++ app/src/main/java/com/example/alc5dayschallenge/AboutAlcActivity.java
@@ -1,18 +1,26 @@
 package com.example.alc5dayschallenge;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AboutAlcActivity extends AppCompatActivity {
 
     WebView webView;
+    ProgressBar progressBar;
     private static String AndelaUrl = "http://andela.com/alc/";
 
     @Override
@@ -28,18 +36,42 @@ public class AboutAlcActivity extends AppCompatActivity {
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         webView.loadUrl(AndelaUrl);
 
+        progressBar = findViewById(R.id.progressBar3);
+
+
     }
 
     private class MyAndelaLoader extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
            view.loadUrl(url);
+           progressBar.setVisibility(View.VISIBLE);
            return true;
         }
 
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
             handler.proceed();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            new Timer().schedule(
+                    new TimerTask() {
+                        @Override
+                        public void run() {
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    },
+                    1400
+            );
         }
     }
 }
